6713adaf75e6b89d36e57633b31a3cb153ddd894
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2022-08-29 11:25:53 -0300

    Add greetings for multiple people

--- first-app/greetings/greetings.go
+++ first-app/greetings/greetings.go
@@ -19,6 +19,28 @@ func Hello(name string) (string, error) {
     return message, nil
 }
 
+// Hellos returns a map that associates each of the named
+// people with a greeting message
+func Hellos(names []string) (map[string]string, error) {
+	// A map to associate names with messages.
+	// make(map[key-type]value-type
+	messages := make(map[string]string)
+
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
--- first-app/hello/hello.go
+++ first-app/hello/hello.go
@@ -10,11 +10,14 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Agustin")
-	
+	// A slice of names
+	names := []string{"Agustin", "Alejandra", "Miqueas"}
+
+	messages, err := greetings.Hellos(names)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	
-	fmt.Println(message)
+	fmt.Println(messages)
 }
